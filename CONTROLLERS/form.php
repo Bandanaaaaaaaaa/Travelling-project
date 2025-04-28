aab8a09afee0376539a2d3e3c939b040171d6c24
Manprabesh Boruah <billionaireboruah31@gmail.com>
2025-04-28 10:57:37 +0530

    user data saved during signup

--- CONTROLLERS/form.php
+++ CONTROLLERS/form.php
@@ -1,9 +1,60 @@
+<?php
+//get the data save into database;
+//set cookie
 
 
-    <?php
-        //get the data save into database
-        //set cookie
-        include(__DIR__."/../VIEWS/profile.html");
-        
-        
-    ?>
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $servername = 'localhost';
+    $username = 'user';
+    $password = "@Manp2003";
+    $dbname = "Travelling";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("connection failed" . mysqli_connect_error());
+    }
+   
+
+
+    $signup_table = "
+CREATE TABLE IF NOT EXISTS signup(
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+email VARCHAR(30) NOT NULL,
+password TEXT NOT NULL
+)";
+
+    $table = mysqli_query($conn, $signup_table);
+    if ($table) {
+
+        echo "Table created successfullt<br>";
+    } else {
+        echo "no table";
+    }
+
+    if (isset($_POST["register"])) {
+        echo "register";
+    }
+    function insertQuery($conn, $data, $signup_table)
+    {
+        $key = implode(", ", array_keys($data));
+        $value =  "'" .implode("','", array_values($data)) ."'";
+        $insert = "INSERT INTO $signup_table ($key) VALUES ($value)";
+        mysqli_query($conn, $insert);
+    }
+
+
+    $data = [
+        "username" => $_POST["username"],
+        "email" => $_POST["email"],
+        "password" => $_POST["password"],
+    ];
+
+    insertQuery($conn, $data, "signup");
+
+
+
+
+    include(__DIR__ . "/../VIEWS/profile.html");
+}
+?>
