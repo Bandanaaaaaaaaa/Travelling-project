7d4ec03f19a54c4181e59f247cadbb60134e89e3
Manprabesh Boruah <billionaireboruah31@gmail.com>
2025-04-29 15:43:03 +0530

    main file:

--- index.php
+++ index.php
@@ -13,23 +13,22 @@ switch ($request) {
         require __DIR__ . $viewDir . 'form.php';
         break;
     case '/signup':
- if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        require __DIR__ . $viewDir . 'signup.html';
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require __DIR__ . $controllerDir . 'form.php';
-    } else {
-        http_response_code(405); // Method Not Allowed
-        echo "Method Not Allowed";
-    }   
-     break;
-case '/login':
-if($_SERVER["REQUEST_METHOD"]==='GET'){
-require __DIR__ . $viewDir. 'login.html';
-}
-elseif ($_SERVER["REQUEST_METHOD"]==="POST"){
-require __DIR__.$controllerDir."login.php";
-}
-break;
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            require __DIR__ . $viewDir . 'signup.html';
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            require __DIR__ . $controllerDir . 'signup.php';
+        } else {
+            http_response_code(405); // Method Not Allowed
+            echo "Method Not Allowed";
+        }
+        break;
+    case '/login':
+        if ($_SERVER["REQUEST_METHOD"] === 'GET') {
+            require __DIR__ . $viewDir . 'login.html';
+        } elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
+            require __DIR__ . $controllerDir . "login.php";
+        }
+        break;
     default:
         http_response_code(404);
         require __DIR__ . $viewDir . '404.php';
